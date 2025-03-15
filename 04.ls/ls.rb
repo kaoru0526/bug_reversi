115e91b4de20740564563b5534871e361a7c48a2
“kaoru.am” <intel.uuuk@gmail.com>
2025-03-15 20:12:08 +0900

    ls.rbで最大3列と8列を表示、3列目に空欄を追加

---
+++ 04.ls/ls.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# !/usr/bin/env ruby
+
+def list_files(directory, columns)
+  files = Dir.entries(directory) - ['.', '..']
+  print_files(files, columns)
+end
+
+def print_files(files, columns)
+  files.each_slice(columns) do |slice|
+    puts slice.map { |file| file.ljust(20) }.join(' ')
+  end
+end
+
+list_files(Dir.pwd, 3)
